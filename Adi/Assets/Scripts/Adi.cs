b6e8e898e357b9e4630c815aca1cc09f790943f2
Nico <n.camil@free.fr>
2022-08-17 12:57:11 +0200

    SPA added

--- Adi/Assets/Scripts/Adi.cs
+++ Adi/Assets/Scripts/Adi.cs
@@ -1,17 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using F4SharedMem;
 using F4SharedMem.Headers;
-using F4Utils.SimSupport;
-using log4net.Core;
-using UnityEditor;
+using SpaNs;
+using System;
+using UnityEngine.UIElements.Experimental;
 
 public class Adi : MonoBehaviour
 {
 
     //debug
     [SerializeField] bool useDebug = false;
+
     //Ball
     [SerializeField] float pitchAngle;
     [SerializeField] float rollAngle;
@@ -31,6 +30,13 @@ public class Adi : MonoBehaviour
     [SerializeField] bool showLocFlag;
     [SerializeField] bool showOffFlag;
     [SerializeField] bool showAuxFlag;
+
+    [SerializeField] float heading;
+
+    //Own aricraft
+    private GameObject _aircarftGameObject;
+    private GameObject _sunGameObject;
+    private GameObject _sunLightGameObject;
 
     //Ball
     private GameObject _BallGameObject;
@@ -57,29 +63,30 @@ public class Adi : MonoBehaviour
     private readonly Reader _sharedMemReader = new();
     private FlightData _lastFlightData;
 
-    private readonly IIndicatedRateOfTurnCalculator _rateOfTurnCalculator = new IndicatedRateOfTurnCalculator();
-
     private const float GLIDESLOPE_SCALE = 5.0f;
     private const float LOCALIZER_SCALE = 1.0f;
     private const float RADIANS_PER_DEGREE = 0.0174532925f;
-    private const float DEGREES_PER_RADIAN = 57.2957795f;
 
     private Camera _cam;
     private float _camera_x_scale = 1.0f;
     private float _mouse_prev_scroll = 0;
-    private Matrix4x4 defaultMatrix = new(
+    private Matrix4x4 _DefaultViewMatrix = new(
                                         new Vector4(4.166667f, 0.0f, 0.0f, 0.0f),
                                         new Vector4(0.0f, 4.166667f, 0.0f, 0.0f),
                                         new Vector4(0.0f, 0.0f, -1.0006f, -1.0f),
                                         new Vector4(0.0f, 0.0f, -0.60018f, 0.0f)
                                     );
+    private Quaternion _DefaultSunRotation = Quaternion.Euler(new Vector3(-122.764f, 23.26199f, 0));
+    private Quaternion _DefaultSunLightRotation = Quaternion.Euler(new Vector3(4.206f, 182.155f, - 176.991f));
+
+    private readonly Spa SpaObject = new();
 
     private FlightData ReadSharedMem()
     {
         return _lastFlightData = _sharedMemReader.GetCurrentData();
     }
 
-    private float DegToRadians(float angle)
+    private float RadToDeg(float angle)
     {
         return angle / RADIANS_PER_DEGREE;
     }
@@ -97,127 +104,39 @@ public class Adi : MonoBehaviour
         _FlagLocGameObject = GameObject.Find("FlagLoc");
         _FlagOffGameObject = GameObject.Find("FlagOff");
         _FlagAuxGameObject = GameObject.Find("FlagAux");
+        _aircarftGameObject = GameObject.Find("Aircraft");
+        _sunGameObject = GameObject.Find("Sun");
+        _sunLightGameObject = GameObject.Find("LightSun");
 
         _cam = Camera.main;
-        
         LoadProjectionMatrix();
+
+        SpaObject.spa.year = 2022;
+        SpaObject.spa.month = 8;
+        SpaObject.spa.day = 1;
+        SpaObject.spa.timezone = +9;
+        SpaObject.spa.function = SpaFunction.ZA;
+
+
     }
 
 
     void Update()
     {
         UpdateAspectRation();
+
+        Update_spa();
+
+        //Update Adi
+        UpdateAdi();
         
-
-        if (useDebug)
-        {
-            _BallGameObject.transform.rotation = Quaternion.Euler(new Vector3(rollAngle, -90, pitchAngle));
-            _bankGameObject.transform.rotation = Quaternion.Euler(new Vector3(0, 0, rollAngle));
-
-            _SlipGameObject.transform.position = new Vector3(slip, _SlipGameObject.transform.position.y, _SlipGameObject.transform.position.z);
-            _TurnRateGameObject.transform.position = new Vector3(turnRate * 2, _TurnRateGameObject.transform.position.y, _TurnRateGameObject.transform.position.z);
-
-
-            _GsGameObject.transform.rotation = Quaternion.Euler(new Vector3(glideAngle, 0, 0));
-            _LocGameObject.transform.rotation = Quaternion.Euler(new Vector3(0, locAngle, 0));
-
-            if (showGsFlag)
-            {
-                _FlagGsGameObject.transform.rotation = Quaternion.Euler(new Vector3(0, 0, -30));
-            }
-            else
-            {
-                _FlagGsGameObject.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
-            }
-
-            if (showLocFlag)
-            {
-                _FlagLocGameObject.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 30));
-            }
-            else
-            {
-                _FlagLocGameObject.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
-            }
-
-            if (showOffFlag)
-            {
-                _FlagOffGameObject.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 30));
-            }
-            else
-            {
-                _FlagOffGameObject.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
-            }
-
-            if (showOffFlag || showAuxFlag)
-            {
-                _FlagAuxGameObject.transform.rotation = Quaternion.Euler(new Vector3(0, 0, -30));
-            }
-            else
-            {
-                _FlagAuxGameObject.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
-            }
-        }
-        else
-        {
-            if (ReadSharedMem() != null)
-            {
-                _BallGameObject.transform.rotation = Quaternion.Euler(new Vector3(-DegToRadians(_lastFlightData.roll), -90, DegToRadians(-_lastFlightData.pitch)));
-                _bankGameObject.transform.rotation = Quaternion.Euler(new Vector3(0, 0, -DegToRadians(_lastFlightData.roll)));
-
-                _SlipGameObject.transform.position = new Vector3(-_lastFlightData.beta, _SlipGameObject.transform.position.y, _SlipGameObject.transform.position.z);
-
-                _GsGameObject.transform.rotation = Quaternion.Euler(new Vector3(GLIDESLOPE_SCALE * DegToRadians(-_lastFlightData.AdiIlsVerPos), 0, 0));
-                _LocGameObject.transform.rotation = Quaternion.Euler(new Vector3(0, LOCALIZER_SCALE * DegToRadians(-_lastFlightData.AdiIlsHorPos), 0));
-
-                var rateOfTurn = _rateOfTurnCalculator.DetermineIndicatedRateOfTurn(_lastFlightData.yaw * DEGREES_PER_RADIAN);
-                _TurnRateGameObject.transform.position = new Vector3(rateOfTurn * 2, _TurnRateGameObject.transform.position.y, _TurnRateGameObject.transform.position.z);
-
-                var hsiBits = (HsiBits)_lastFlightData.hsiBits;
-                bool _showAuxFlag = ((hsiBits & HsiBits.ADI_AUX) == HsiBits.ADI_AUX) || ((hsiBits & HsiBits.ADI_OFF) == HsiBits.ADI_OFF);
-                bool _showOffFlag = (hsiBits & HsiBits.ADI_OFF) == HsiBits.ADI_OFF;
-                bool _showGsFlag = (hsiBits & HsiBits.ADI_GS) == HsiBits.ADI_GS;
-                bool _showLocFlag = (hsiBits & HsiBits.ADI_LOC) == HsiBits.ADI_LOC;
-
-                if (_showGsFlag)
-                {
-                    _FlagGsGameObject.transform.rotation = Quaternion.Euler(new Vector3(0, 0, -30));
-                }
-                else
-                {
-                    _FlagGsGameObject.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
-                }
-
-                if (_showLocFlag)
-                {
-                    _FlagLocGameObject.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 30));
-                }
-                else
-                {
-                    _FlagLocGameObject.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
-                }
-
-                if (_showOffFlag)
-                {
-                    _FlagOffGameObject.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 30));
-                }
-                else
-                {
-                    _FlagOffGameObject.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
-                }
-
-                if (_showAuxFlag)
-                {
-                    _FlagAuxGameObject.transform.rotation = Quaternion.Euler(new Vector3(0, 0, -30));
-                }
-                else
-                {
-                    _FlagAuxGameObject.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
-                }
-            }
-        }
-
     }
 
+
+    private void OnApplicationQuit()
+    {
+        SaveProjectionMatrix();
+    }
 
     //change window aspect ration
     private void UpdateAspectRation()
@@ -237,12 +156,12 @@ public class Adi : MonoBehaviour
         {
             for (int j = 0; j < 4; j++)
             {
-                defaultMatrix[i,j] = PlayerPrefs.GetFloat("M"+i+"_"+j, defaultMatrix[i,j]);
+                _DefaultViewMatrix[i,j] = PlayerPrefs.GetFloat("M"+i+"_"+j, _DefaultViewMatrix[i,j]);
                 
             }
         }
 
-        _cam.projectionMatrix = defaultMatrix;
+        _cam.projectionMatrix = _DefaultViewMatrix;
     }
 
     private void SaveProjectionMatrix()
@@ -258,8 +177,153 @@ public class Adi : MonoBehaviour
     }
 
 
-    private void OnApplicationQuit()
+    private void UpdateAdi()
     {
-        SaveProjectionMatrix();
+        if (useDebug)
+        {
+            _aircarftGameObject.transform.localRotation = Quaternion.Euler(new Vector3(pitchAngle, 180+heading, rollAngle));
+
+            _BallGameObject.transform.localRotation = Quaternion.Euler(new Vector3(pitchAngle, 0, rollAngle));
+            _bankGameObject.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, rollAngle));
+
+            _SlipGameObject.transform.localPosition = new Vector3(slip, _SlipGameObject.transform.localPosition.y, _SlipGameObject.transform.localPosition.z);
+            _TurnRateGameObject.transform.localPosition = new Vector3(turnRate * 2, _TurnRateGameObject.transform.localPosition.y, _TurnRateGameObject.transform.localPosition.z);
+
+
+            _GsGameObject.transform.localRotation = Quaternion.Euler(new Vector3(glideAngle, 0, 0));
+            _LocGameObject.transform.localRotation = Quaternion.Euler(new Vector3(0, locAngle, 0));
+
+            if (showGsFlag)
+            {
+                _FlagGsGameObject.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, -30));
+            }
+            else
+            {
+                _FlagGsGameObject.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, 0));
+            }
+
+            if (showLocFlag)
+            {
+                _FlagLocGameObject.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, 30));
+            }
+            else
+            {
+                _FlagLocGameObject.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, 0));
+            }
+
+            if (showOffFlag)
+            {
+                _FlagOffGameObject.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, 30));
+            }
+            else
+            {
+                _FlagOffGameObject.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, 0));
+            }
+
+            if (showOffFlag || showAuxFlag)
+            {
+                _FlagAuxGameObject.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, -30));
+            }
+            else
+            {
+                _FlagAuxGameObject.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, 0));
+            }
+        }
+        else
+        {
+            if (ReadSharedMem() != null)
+            {
+                _aircarftGameObject.transform.localRotation = Quaternion.Euler(new Vector3(RadToDeg(_lastFlightData.pitch), 180 + _lastFlightData.currentHeading, -RadToDeg(_lastFlightData.roll)));
+
+
+                _BallGameObject.transform.localRotation = Quaternion.Euler(new Vector3(RadToDeg(_lastFlightData.pitch), 0, -RadToDeg(_lastFlightData.roll)));
+                _bankGameObject.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, -RadToDeg(_lastFlightData.roll)));
+
+                _SlipGameObject.transform.localPosition = new Vector3(_lastFlightData.beta, _SlipGameObject.transform.localPosition.y, _SlipGameObject.transform.localPosition.z);
+
+                _GsGameObject.transform.localRotation = Quaternion.Euler(new Vector3(GLIDESLOPE_SCALE * RadToDeg(-_lastFlightData.AdiIlsVerPos), 0, 0));
+                _LocGameObject.transform.localRotation = Quaternion.Euler(new Vector3(0, LOCALIZER_SCALE * RadToDeg(-_lastFlightData.AdiIlsHorPos), 0));
+
+                _TurnRateGameObject.transform.localPosition = new Vector3(_lastFlightData.turnRate * 0.4f, _TurnRateGameObject.transform.localPosition.y, _TurnRateGameObject.transform.localPosition.z);
+
+                var hsiBits = (HsiBits)_lastFlightData.hsiBits;
+                bool _showAuxFlag = ((hsiBits & HsiBits.ADI_AUX) == HsiBits.ADI_AUX) || ((hsiBits & HsiBits.ADI_OFF) == HsiBits.ADI_OFF);
+                bool _showOffFlag = (hsiBits & HsiBits.ADI_OFF) == HsiBits.ADI_OFF;
+                bool _showGsFlag = (hsiBits & HsiBits.ADI_GS) == HsiBits.ADI_GS;
+                bool _showLocFlag = (hsiBits & HsiBits.ADI_LOC) == HsiBits.ADI_LOC;
+
+                if (_showGsFlag)
+                {
+                    _FlagGsGameObject.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, -30));
+                }
+                else
+                {
+                    _FlagGsGameObject.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, 0));
+                }
+
+                if (_showLocFlag)
+                {
+                    _FlagLocGameObject.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, 30));
+                }
+                else
+                {
+                    _FlagLocGameObject.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, 0));
+                }
+
+                if (_showOffFlag)
+                {
+                    _FlagOffGameObject.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, 30));
+                }
+                else
+                {
+                    _FlagOffGameObject.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, 0));
+                }
+
+                if (_showAuxFlag)
+                {
+                    _FlagAuxGameObject.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, -30));
+                }
+                else
+                {
+                    _FlagAuxGameObject.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, 0));
+                }
+            }
+        }
+
+    }
+
+    //Sun position
+    private void Update_spa()
+    {
+        if (useDebug)
+        {
+            _sunGameObject.transform.rotation = _DefaultSunRotation;
+            _sunLightGameObject.transform.rotation = _DefaultSunLightRotation;
+        }
+        else
+        {
+            if (ReadSharedMem() != null)
+            {
+                TimeSpan t = TimeSpan.FromSeconds(_lastFlightData.currentTime);
+                SpaObject.spa.hour = t.Hours;
+                SpaObject.spa.minute = t.Minutes;
+                SpaObject.spa.second = t.Seconds;
+
+                SpaObject.spa.longitude = _lastFlightData.longitude;
+                SpaObject.spa.latitude = _lastFlightData.latitude;
+
+                SpaObject.spa.year = 2022;
+                SpaObject.spa.month = 8;
+                SpaObject.spa.day = 1;
+                SpaObject.spa.timezone = +9;
+                SpaObject.spa.function = SpaFunction.ZA;
+
+                SpaObject.Spa_calculate();
+
+                _sunGameObject.transform.rotation = Quaternion.Euler(new Vector3(-(float)SpaObject.spa.zenith, (float)SpaObject.spa.azimuth, 0));
+            }
+        }
+
+        _sunLightGameObject.transform.LookAt(_BallGameObject.transform);
     }
 }
